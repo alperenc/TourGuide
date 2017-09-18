ac5d0f77055ece2d40a19b8bd84e958b82b6c114
Alp Eren Can <alperenc@gmail.com>
2017-09-18 11:27:10 +0300

    refactor boilerplate PlaceholderFragment into AttractionFragment

--- app/src/main/java/com/alperencan/tourguide/android/AttractionsPagerAdapter.java
+++ app/src/main/java/com/alperencan/tourguide/android/AttractionsPagerAdapter.java
@@ -22,7 +22,7 @@ public class AttractionsPagerAdapter extends FragmentPagerAdapter {
     public Fragment getItem(int position) {
         // getItem is called to instantiate the fragment for the given page.
         // Return a PlaceholderFragment (defined as a static inner class below).
-        return TourGuideActivity.PlaceholderFragment.newInstance(position + 1);
+        return AttractionFragment.newInstance(position + 1);
     }
 
     @Override
