94fde05b1bc7e390c81d6bb79d718157ffa96845
Alp Eren Can <alperenc@gmail.com>
2017-09-18 22:34:08 +0300

    renamed AttractionItem to AttractionLocation

--- app/src/main/java/com/alperencan/tourguide/android/AttractionLocation.java
+++ app/src/main/java/com/alperencan/tourguide/android/AttractionLocation.java
@@ -1,11 +1,11 @@
 package com.alperencan.tourguide.android;
 
 /**
- * {@AttractionItem} represents an attraction location that the user would like to visit.
+ * {@AttractionLocation} represents an attraction location that the user would like to visit.
  * It contains resource IDs for title, address, and optional image file for that attraction.
  */
 
-public class AttractionItem {
+public class AttractionLocation {
 
     /**
      * Constant value that represents no image was provided for this attraction
@@ -28,24 +28,24 @@ public class AttractionItem {
     private int imageResourceId = NO_IMAGE_PROVIDED;
 
     /**
-     * Create a new AttractionItem object.
+     * Create a new AttractionLocation object.
      *
      * @param titleId   is the string resource ID for the attraction title
      * @param addressId is the string resource ID for the attraction address
      */
-    public AttractionItem(int titleId, int addressId) {
+    public AttractionLocation(int titleId, int addressId) {
         this.titleId = titleId;
         this.addressId = addressId;
     }
 
     /**
-     * Create a new AttractionItem object.
+     * Create a new AttractionLocation object.
      *
      * @param titleId         is the string resource ID for the attraction title
      * @param addressId       is the string resource ID for the attraction address
      * @param imageResourceId is the resource ID for the image associated with the attraction
      */
-    public AttractionItem(int titleId, int addressId, int imageResourceId) {
+    public AttractionLocation(int titleId, int addressId, int imageResourceId) {
         this(titleId, addressId);
         this.imageResourceId = imageResourceId;
     }
